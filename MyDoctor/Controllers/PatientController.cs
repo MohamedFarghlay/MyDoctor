b8388a864c48ec213e743e3f2fad8c5c2d16984f
MohamedFarghlay <muhamad.farghly@gmail.com>
2019-11-23 13:50:15 +0200

    Add Patient Session

--- MyDoctor/Controllers/PatientController.cs
+++ MyDoctor/Controllers/PatientController.cs
@@ -15,14 +15,24 @@ namespace MyDoctor.Controllers
         // GET: Patient
         MyDoctorDBContext DoctorDBContext = new MyDoctorDBContext();
         MyDoctorRepository doctordb = new MyDoctorRepository();
-
-        public ActionResult PatientDashboard()
+        Patient patientUser = new Patient();
+      
+        //Get : Dashboard 
+        public ActionResult PatientDashboard(string userName)
         {
-            return View();
+            if (Session["username"] == null)
+            {
+                return View("LogIn");
+            }
+            else {
+                ViewBag.userName = userName;
+                return View();
+            }
         }
 
+
         //Get : MyProfile
-        public ActionResult MyProfile(int? id)
+        public ActionResult MyProfile(int? id=20)
         {
             if (id != null)
             {
@@ -39,40 +49,38 @@ namespace MyDoctor.Controllers
                 }
                 else
                 {
-                  return RedirectToAction("Error", "Home");
+                    return RedirectToAction("Error", "Home");
                 }
             }
 
-          return RedirectToAction("Error", "Home");
+            return RedirectToAction("Error", "Home");
 
 
 
         }
 
-        
-        
+
         //Get  : Edit
         public ActionResult Edit(int? id)
         {
-            
+
             EmailExist.flag = true;
 
             var user = DoctorDBContext.Users.Where(u => u.ID == id).FirstOrDefault();
             if (user != null)
-            { 
-            return View(user);
+            {
+                return View(user);
             }
-          return RedirectToAction("Error", "Home");
+            return RedirectToAction("Error", "Home");
         }
-
 
         //Post : Edit
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit( User user)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Edit(User user)
         {
 
-            
+
             User targetsUser = new MyDoctorDB.User();
             targetsUser = DoctorDBContext.Users.Where(u => u.ID == user.ID).FirstOrDefault();
             if (targetsUser != null)
@@ -95,9 +103,6 @@ namespace MyDoctor.Controllers
 
         }
 
-
-       
-
         //Get : Register
         public ActionResult PatientRegister()
         {
@@ -106,33 +111,91 @@ namespace MyDoctor.Controllers
 
         //Post : PatientRegiser
         [HttpPost]
+        //[ValidateAntiForgeryToken]
         public ActionResult PatientRegister(User user)
         {
 
-            User newUser = new MyDoctorDB.User();
-
+            User newUser = new User();
+            Patient patient = new Patient();
             if (ModelState.IsValid)
             {
-
-                //new patient
-                user.PatientID++;
-
-                //encrypt the password
                 string EncryptedPassword = EncryptPassword.encryptPassword(user.Password);
-                //string decryptedPassword = DecryptPassword.decryptPassword(EncryptedPassword);
-                newUser.FirstName = user.FirstName;
-                newUser.LastName = user.LastName;
-                newUser.Email = user.Email;
-                newUser.PhoneNumner = user.PhoneNumner;
+                user.Password = EncryptedPassword;
+                user.PatientID = 1;
 
-                newUser.Password = EncryptedPassword;
-                newUser.gender = user.gender;
-                newUser.DateOfBirth = user.DateOfBirth;
-                doctordb.SetUser(newUser);
+                patient.users = user;
+                patient.PatientID = 0;
+                doctordb.SetPatient(patient);
+
+              
+
+                //doctordb.UpdateUser(user);
+                //user.PatientID = patientID;
+                //user.DoctorID = 0;
+                ////new patient
+                ////user.PatientID++;
+                ////encrypt the password
+                //string EncryptedPassword = EncryptPassword.encryptPassword(user.Password);
+                ////string decryptedPassword = DecryptPassword.decryptPassword(EncryptedPassword);
+                //newUser.FirstName = user.FirstName;
+                //newUser.LastName = user.LastName;
+                //newUser.Email = user.Email;
+                //newUser.PhoneNumner = user.PhoneNumner;
+                //newUser.Password = EncryptedPassword;
+                //newUser.gender = user.gender;
+                ////newUser.DateOfBirth = user.DateOfBirth;
+                //doctordb.SetUser(newUser);
 
                 return View("PatientDashboard");
             }
             return View(user);
+        }
+
+
+        //Get :LogIn
+        public ActionResult LogIn()
+        {
+            //if user is already logged in -> view it's dashboard directly
+            if (Session["username"] != null)
+            {
+                ViewBag.title = "Dashboard";
+                return View("PatientDashboard", new { username = Session["username"].ToString() });
+            }
+
+            
+            return View("LogIn");
+        }
+
+        //Post : LogIn
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult LogIn(UserLogIn user)
+        {
+            if ((user.Password == null) || (user.Email == null))
+            {
+                return View(user);
+            }
+
+            string pass = EncryptPassword.encryptPassword(user.Password);
+
+
+            //Check The Existance of the user
+
+            var userLoggedIn = DoctorDBContext.Users.SingleOrDefault(u => u.Email == user.Email && u.Password == pass);
+            if (userLoggedIn != null) //found the user
+            {
+                ViewBag.message = "Logged In ";
+                ViewBag.triedOnce = "Yes";
+
+                Session["username"] = userLoggedIn.FirstName;
+                return View("PatientDashboard", new { username = userLoggedIn.FirstName });
+            }
+            else
+            {
+                ViewBag.triedOnce = "Yes";
+               
+                return View();
+            }
         }
 
         //Get : ConfirmationDeleting
@@ -141,13 +204,14 @@ namespace MyDoctor.Controllers
             return View();
         }
 
+
         //Get : Delete
-        public ActionResult Delete(int?id)
+        public ActionResult Delete(int? id)
         {
             if (id != null)
             {
-                var deletedUser = DoctorDBContext.Users.Where(u=>u.ID== id).FirstOrDefault();
-                if(deletedUser!= null)
+                var deletedUser = DoctorDBContext.Users.Where(u => u.ID == id).FirstOrDefault();
+                if (deletedUser != null)
                 {
                     return View(deletedUser);
                 }
@@ -156,20 +220,22 @@ namespace MyDoctor.Controllers
             return View("Error");
         }
 
-            
-
         //Post : Delete
         [HttpPost]
         //[ValidateAntiForgeryToken]
         public ActionResult Delete(User user)
         {
-            User deletedUser = DoctorDBContext.Users.Where(u=>u.ID==user.ID).FirstOrDefault();
+            User deletedUser = DoctorDBContext.Users.Where(u => u.ID == user.ID).FirstOrDefault();
             if (deletedUser != null)
             {
+                //First Delete The Patient That Hold this user
+                doctordb.DeletePatient(deletedUser.ID);
+
+                //then delete the user
                 doctordb.DeleteUser(deletedUser);
                 return RedirectToAction("Display", "Test");
             }
-            
+
             return RedirectToAction("Error", "Home");
 
         }
--- MyDoctor/MyDoctorDB/User.cs
+++ MyDoctor/MyDoctorDB/User.cs
@@ -19,7 +19,6 @@ namespace MyDoctor.MyDoctorDB
     {
         //ID Vaildation 
         [Key]
-        [OnlyInt(ErrorMessage = "This Valid Must accept only integers")]
         public int ID { get; set; }
 
         //Firat Name Validation
@@ -51,7 +50,7 @@ namespace MyDoctor.MyDoctorDB
 
         //Password Validation
         [Required(ErrorMessage ="Password is Required")]
-        [PasswordValidation(ErrorMessage ="Password Must Contains Capital, Small Letters,  And Numbers")]
+        //[PasswordValidation(ErrorMessage ="Password Must Contains Capital, Small Letters,  And Numbers")]
         [StringLength(100,ErrorMessage ="The length of Password must be at least 6",MinimumLength =6)]
         public string Password { get; set; }
 
